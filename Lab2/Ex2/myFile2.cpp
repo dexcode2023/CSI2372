0fbd15b38e953532dc59a066217ced15b6d3a0ed
dexcode2023 <dextreday@gmail.com>
2024-09-30 21:34:39 -0400

    setting up lab 2 exercises

---
+++ Lab2/Ex2/myFile2.cpp
@@ -0,0 +1,24 @@
+/*myFile2.cpp : Ex2 a2 CSI2372A*/
+
+#include "myFile2.h"
+
+int main() {
+	int myArray[sizeArray] = { 2,4,88,20,3,55,87,134,2,5 };
+
+	cout << "Displaying the unsorted array :" << endl;
+	for (int i = 0; i < sizeArray; i++) {
+		cout << myArray[i] << " ";
+	}
+	cout <<  endl << endl;
+	sort(myArray, sizeArray);
+	cout << "Displaying the sorted array :" << endl;
+	for (int i = 0; i < sizeArray; i++) {
+		cout << myArray[i] << " ";
+	}
+	cout << endl;
+}
+
+void sort(int a[], int size)
+{
+	//YOUR CODE HERE
+}
---
+++ Lab2/Ex2/myFile2.h
@@ -0,0 +1,7 @@
+/*myFile2.h : Ex2 a2 CSI2372A*/
+
+
+#include <iostream>
+using namespace std;
+const int sizeArray = 10;	//number of items in the array
+
