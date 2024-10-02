fc20eb6a1ce792d16dc693114ef19fb47d7decb4
wo874 <92176181+wo874@users.noreply.github.com>
2024-10-01 22:14:15 -0400

    Update myfile2.cpp

--- Lab1/lab1/lab1/myfile2.cpp
+++ Lab1/lab1/lab1/myfile2.cpp
@@ -1,6 +1,7 @@
-// Q1. The main function displays a menu and calculates areas/volumes.
+// Q1. The main function displays the option to calculate disk, cylinder or exit.
 
 //Q2. 
+//Note: I modified the annex to be slighty more polite
 #include <iostream>
 #include <cmath>
 #include "myfile2.h"  
@@ -25,13 +26,9 @@ int main() {
         cout <<" 𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽𓇽\n";
 
         cout << "What would you like to do?\n";
-
         cout << "Calculate the area of a disk from a radius (Press 1)\n";
-
         cout << "Calculate the volume of a cylinder from a radius and a height (Press 2)\n";
-
         cout << "Exit the program (Press 3)\n";
-
         cout << "Please enter your choice? ";
 
         cin >> choice;
@@ -40,10 +37,8 @@ int main() {
 
             case 1:
                 cout << "Surface calculation\n";
-
                 cout << "Please enter the radius? ";
                 cin >> radius;
-
                 cout << "Boom! The surface is: " << surface(radius) << endl;
                 surfaceCount++;
 
@@ -51,13 +46,10 @@ int main() {
 
             case 2:
                 cout << "Volume calculation\n";
-
                 cout << "Please enter the radius: ";
                 cin >> radius;
-
                 cout << "Please enter the height: ";
                 cin >> height;
-
                 cout << "Tada! The volume is: " << volume(radius, height) << endl;
                 volumeCount++;
                 break;
