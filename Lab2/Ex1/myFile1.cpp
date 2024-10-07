96e0650ad4e82d51f5e2a793d53590560dcc2ff9
wo874 <92176181+wo874@users.noreply.github.com>
2024-10-07 18:38:35 -0400

    Update myFile1.cpp

--- Lab2/Ex1/myFile1.cpp
+++ Lab2/Ex1/myFile1.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    int tab[10];  
+    int* p;       
+
+    for (int i = 0; i < 10; i++) {
+        tab[i] = i * i;
+    }
+
+    cout << tab[2] << endl;  // Print the value of tab[2] 
+
+    tab[2] = tab[1];         // Set tab[2] to the value of tab[1] 
+    cout << tab[2] << endl;  // Print the value of tab[2] 
+
+    tab[2] = *(tab + 1);     // Set tab[2] to the value at tab[1] 
+    cout << tab[2] << endl;  // Print the value of tab[2] 
+
+    *(tab + 2) = tab[1];     // Set the value at tab[2] to tab[1] 
+    cout << *(tab + 2) << endl;  // Print the value at tab[2] 
+
+    *(tab + 2) = *(tab + 1); // Set tab[2] to the value at tab[1] 
+    cout << *(tab + 2) << endl;  // Print the value at tab[2] 
+
+    p=&tab[0];             // Point p to the start of the array
+    p=tab + 1;             // Point p to tab[1]
+    
+    tab[4] = *p;             // Set tab[4] to the value pointed by p 
+    cout << tab[4] << endl;  // Print the value of tab[4] 
+    
+    return 0;
+}
 
