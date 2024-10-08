d36fe4cda1d2759b7351b154552dcfc395700e30
dexcode2023 <dextreday@gmail.com>
2024-10-07 23:31:22 -0400

    finishing ex3 for lab2, pascal triangle generator

--- Lab2/Ex3/myFile3.cpp
+++ Lab2/Ex3/myFile3.cpp
@@ -6,6 +6,33 @@ using namespace std;
 int** triangleInf(int n)
 {
 	//YOUR CODE
+	int** result = new int*[n];
+	int* last;
+	for(int i = 0; i < n; i++){
+		result[i] = new int[i+1];
+		if(i == 0){
+			result[i][0] = 1;
+			last = result[i];
+		} else{
+			for(int j = 0; j < i+1; j++){
+				if(j==0){
+					result[i][0] = 1;
+				} else if(j==i){
+					result[i][j] =1;
+				} else{
+					result[i][j] = last[j-1]+last[j];
+				}
+				
+
+			}
+			last = result[i];
+
+		}
+
+
+
+	}
+	return result;
 }
 
 int main() {
