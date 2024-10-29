536078084250477e62a73f7a7743f6d92094831d
dexcode2023 <dextreday@gmail.com>
2024-10-29 19:00:21 -0400

    adding lab 4

---
+++ Lab4/Ex2/myFile2.cpp
@@ -0,0 +1,68 @@
+/*Ex2 devoir4 CSI2772A*/
+
+
+#include <iostream> 
+#include "myFile2.h"
+using namespace std;
+
+SetInt::SetInt(int list[], int size) : size(size){
+    elem = new int[size];
+    for(int i = 0; i<size;i++){
+        elem[i] = list[i];
+    }
+
+}
+
+SetInt::~SetInt(){
+    delete[] elem;
+}
+
+SetInt::SetInt(const SetInt& t){
+    size = t.size;
+    elem = new int[size];
+    for(int i = 0; i< size; i++){
+        elem[i] = t.elem[i];
+    }
+}
+
+void SetInt::add(int t){
+    for(int i = 0; i<size; i++){
+        if(t ==*(elem + i)){
+            cout<< "The element that you attempted to add already exists inside the set." <<endl;
+            return;
+
+        }
+    }
+    int* res = new int[size++];
+    for(int i = 0; i < size-1; i++){
+        res[i] = elem[i];
+    }
+    res[size-1] = t;
+    delete[] elem;
+    elem = res;
+}
+
+int main() {
+    SetInt a; 	// object creation
+    int* tab;
+
+    while (a.nbElem() < 5)
+    {
+        cout << "add an int element" << endl;
+        int elem;
+        cin >> elem;
+        a.add(elem);
+
+    }
+    cout << "a contains 10 :" << a.contains(10) << endl;
+    cout << "remove 10 " << endl;
+    a.remove(10);
+    cout << "a contains 10 :" << a.contains(10) << endl;
+    cout << "a contains :" << a.nbElem() << " elements " << endl;
+    tab = a.tabElem();
+    cout << "The elements of a are :" << endl;
+    for (int i =0; i<a.nbElem(); i++)
+        cout << tab[i] << endl;
+
+    return 0;
+}
