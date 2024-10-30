f9a65c59aeb80e1eff6902c2fbf1e00bcb3970c8
wo874 <92176181+wo874@users.noreply.github.com>
2024-10-29 23:23:53 -0400

    Update myFile1a.h

--- Lab4/Ex1/myFile1a.h
+++ Lab4/Ex1/myFile1a.h
@@ -1,16 +1,24 @@
 /*myFile1a, Ex1 a4 CSI2372A*/
 
+#ifndef MYFILE1A_H
+#define MYFILE1A_H
+
 #include <iostream>
 using namespace std;
 
 class Course {
-    int num;
-    int hours;
+    int num;    
+    int hours;  
 public:
-    Course(int, int); 	//constructor
-    int getNum();
-    int getHours();
+    Course(int courseNum, int courseHours) : num(courseNum), hours(courseHours) {}
+    int getNum() {
+        return num;
+    }
+    int getHours() {
+        return hours;
+    }
 };
 
+#endif
 
 
