e97bf14a5a973bdcfbc85c5b4a13ba6093eda0c8
wo874 <92176181+wo874@users.noreply.github.com>
2024-11-05 17:58:40 -0500

    Update myFile.cpp

--- Lab5/myFile.cpp
+++ Lab5/myFile.cpp
@@ -1,100 +1,73 @@
-
 #include <iostream>
-using namespace std;
-#include <cstdlib>
-#include <ctime>
-
 #include "myFile.h"
 #include "Card.h"
-#include "CardsSet.h"
 #include "Player.h"
+#include "CardsSet.h"
 
-int main() {
-	CardsSet packet;
-	Player you(packet, false);
-	Player me(packet, true);
-	char answer[3];
-	bool continuous = true;
-	cout << "Hello! " << endl;
-	while (continuous)
-	{
-		cout << "A new game? " << endl;
-		cin >> answer;
-		continuous = answer[0] == 'y';
-		if (continuous)
-		{
-			packet.novSet();
-			packet.shuffle();
-			packet.take();
-			int p1 = you.play();
-			if (p1 > 21) {
-				cout << "You lost! " << endl;
-			}
-			else if (p1 == 21) {
-				cout << "You won! " << endl;
-			}
-			else // the computer must play
-			{
-				int p2 = me.play();
-				if (p2 <= 21 && p2 >= p1) {
-					cout << "You lost! " << endl;
-				}
-				else {
-					cout << "You won! " << endl;
-				}
-			}
-		}
-	}
-	return 0;
+void Card::write() {
+    std::string colorName;
+    switch (color) {
+        case club: colorName = "Club"; break;
+        case diamond: colorName = "Diamond"; break;
+        case heart: colorName = "Heart"; break;
+        case spade: colorName = "Spade"; break;
+    }
+
+    std::cout << colorName << " ";
+    if (value >= 1 && value <= 10) {
+        std::cout << value;
+    } else if (value == 11) {
+        std::cout << "Jack";
+    } else if (value == 12) {
+        std::cout << "Queen";
+    } else if (value == 13) {
+        std::cout << "King";
+    }
+    std::cout << std::endl;
 }
 
-void CardsSet::novSet() {
-    int x = 0;
-    
-    for (int i = 1; i < 14; ++i) {
-        set[x++] = Card(club, i);
-        set[x++] = Card(diamond, i);
-        set[x++] = Card(heart, i);
-        set[x++] = Card(spade, i);
+void Player::play() {
+    bool wantsMore = true;
+    int totalPoints = 0;
+    cout << "★★★★★★★★★★★★★★★★★★★★★★★★★★★★★" <<;
+    std::cout << "Starting your turn..." << std::endl;
+
+    while (wantsMore && totalPoints <= 21) {
+        Card drawnCard = packet.take();
+        hand.push_back(drawnCard);
+        drawnCard.write();
+
+        totalPoints = countPoints();
+
+        std::cout << "Current total points: " << totalPoints << std::endl;
+        if (totalPoints > 21) {
+            std::cout << "You exceeded 21 points!" << std::endl;
+            break;
+        }
+        char response;
+        std::cout << "Do you want an additional card? (y/n): ";
+        std::cin >> response;
+        wantsMore = (response == 'y');
     }
-    
+}
+int Player::countPoints() {
+    int points = 0;
+    bool hasAce = false;
+    for (const Card& card : hand) {
+        if (card.getValue() == 1) {
+            hasAce = true;
+            points += 14; 
+        } else if (card.getValue() >= 11 && card.getValue() <= 13) {
+            points += 10; 
+        } else {
+            points += card.getValue();
+        }
+    }
+    if (points > 21 && hasAce) {
+        points -= 13; 
+    }
+    return points;
 }
 
-void CardsSet::shuffle(){
-    if (number == 0 || number == 1) {
-        cout << "No cards to shuffle." << endl; 
-    }
-    srand(static_cast<unsigned int>(time(0)));
-    for(int i = 0; i <number;i++){
-        int j = rand() % number;
-        swap(set[i],set[j]);
-    }
-}
-
-Card CardsSet::take(){
-    if(number==0){
-        throw std::out_of_range("Empty card set. Cannot take.");
-    }
-    Card res = set[number - 1];
-    number--;
-    return res;
-}
-
-void CardsSet::put(Card k){
-    if(number == 52){
-        cout<<"Deck is full cannot add card."<<endl;
-        return;
-    }
-    set[number] = k;
-    number++;
-
-}
-
-Card CardsSet::lookIn(int no){
-    if(number-no < 0 ){
-        throw std::out_of_range("Cannot access card out of set bounds.");
-    }
-    return set[number-no];
-}
 
 
