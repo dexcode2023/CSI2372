201ebd70dbc948692517deb94d13da783008c96d
dexcode2023 <dextreday@gmail.com>
2024-11-05 18:57:47 -0500

    Update Card.h

--- Lab5/Card.h
+++ Lab5/Card.h
@@ -1,7 +1,6 @@
 #include <iostream>
 using namespace std;
-#include <cassert> //for assert()
-
+#include <cassert> 
 enum color { club, diamond, heart, spade };
 
 class Card {
@@ -14,10 +13,41 @@ private:
     int val;
 };
 
-/*Constructor*/
 Card::Card(color c, int v) {
-    assert(v >= 1 && v <= 13); //we use a standard function void assert (int expression)
-    //which indicates an error message if the expression is false.
-    col = c;
+    assert(v >= 1 && v <= 13); 
     val = v;
+}
+
+void Card::write() {
+    switch (col) {
+        case club: cout << "Club "; break;
+        case diamond: cout << "Diamond "; break;
+        case heart: cout << "Heart "; break;
+        case spade: cout << "Spade "; break;
+    }
+
+    if (val >= 2 && val <= 10) {
+        cout << val; 
+    } else {
+        switch (val) {
+            case 1: cout << "Ace"; break;
+            case 11: cout << "Jack"; break;
+            case 12: cout << "Queen"; break;
+            case 13: cout << "King"; break;
+        }
+    }
+
+    cout << endl;
+}
+
+int main() {
+    Card card1(heart, 1);  
+    Card card2(spade, 11); 
+    Card card3(diamond, 7); 
+
+    card1.write();
+    card2.write();
+    card3.write();
+
+    return 0;
 }
