1fe0aeea7b7a980efe9d59a68dc711894d40b6d4
dexcode2023 <dextreday@gmail.com>
2024-11-05 16:57:05 -0500

    updating cardset and myfile

--- Lab5/CardsSet.h
+++ Lab5/CardsSet.h
@@ -1,8 +1,3 @@
-#include <cstdlib>
-#include <ctime>
-#include "Card.h"
-
-
 class CardsSet {
 public:
     CardsSet() : number(0) {}
@@ -18,51 +13,3 @@ private:
     int number;
 };
 
-void CardsSet::novSet() {
-    int x = 0;
-    
-    for (int i = 1; i < 14; ++i) {
-        set[x++] = Card(club, i);
-        set[x++] = Card(diamond, i);
-        set[x++] = Card(heart, i);
-        set[x++] = Card(spade, i);
-    }
-    
-}
-
-void CardsSet::shuffle(){
-    if (number == 0 || number == 1) {
-        cout << "No cards to shuffle." << endl; 
-    }
-    srand(static_cast<unsigned int>(time(0)));
-    for(int i = 0; i <number;i++){
-        int j = rand() % number;
-        swap(set[i],set[j]);
-    }
-}
-
-Card CardsSet::take(){
-    if(number==0){
-        throw std::out_of_range("Empty card set. Cannot take.");
-    }
-    Card res = set[number - 1];
-    number--;
-    return res;
-}
-
-void CardsSet::put(Card k){
-    if(number == 52){
-        cout<<"Deck is full cannot add card."<<endl;
-        return;
-    }
-    set[number] = k;
-    number++;
-
-}
-
-Card CardsSet::lookIn(int no){
-    if(number-no < 0 ){
-        throw std::out_of_range("Cannot access card out of set bounds.");
-    }
-    return set[number-no];
-}
