b4562a1538fbe2da5d1f1896145c400399bc1763
dexcode2023 <dextreday@gmail.com>
2024-11-25 19:19:02 -0500

    tradearea

--- Project/TradeArea.h
+++ Project/TradeArea.h
@@ -15,7 +15,46 @@ class TradeArea{
     public:
 
         TradeArea& operator+=(Card* card){
-            
+            tradespace.push_back(card);
+            return *this;
+        }
+
+        bool legal(Card* card) const{
+            for(const auto& carda: tradespace){
+                if(carda->getName() == card->getName()){
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        Card* trade(const std::string& bean){
+
+            for(auto card = tradespace.begin(); card != tradespace.end(); ++card){
+                if((*card)->getName() == bean){
+                    Card* hold = *card;
+                    tradespace.erase(card);
+                    return hold;
+                }
+            }
+            return nullptr;
+        }
+
+        int numCards() const{
+            return tradespace.size();
+        }
+
+        void print(std::ostream& os) const{
+
+            for (const auto& card : tradespace){
+                card->print(os);
+                os<< " ";
+            }
+        }
+
+        friend std::ostream& operator<<(std::ostream& os,const TradeArea& tradearea){
+            tradearea.print(os);
+            return os;
         }
 
 
